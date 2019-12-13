3a98232c4542c0c95014d01fcea258dff9a04d87
Sam Gross <colesbury@gmail.com>
2019-12-13 12:24:03 -0500

    Enable/disable the GIL at runtime
    The GIL is controlled by the environment variable PYTHONGIL or the flag
    "-X nogil". The GIL is enabled by default. The interpreter will
    currently crash when running multi-threaded programs without the GIL.

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -2439,6 +2439,7 @@ static PyStructSequence_Field flags_fields[] = {
     {"hash_randomization",      "-R"},
     {"isolated",                "-I"},
     {"dev_mode",                "-X dev"},
+    {"nogil",                   "-X nogil"},
     {"utf8_mode",               "-X utf8"},
     {0}
 };
@@ -2482,6 +2483,7 @@ make_flags(PyThreadState *tstate)
     SetFlag(config->use_hash_seed == 0 || config->hash_seed != 0);
     SetFlag(config->isolated);
     PyStructSequence_SET_ITEM(seq, pos++, PyBool_FromLong(config->dev_mode));
+    PyStructSequence_SET_ITEM(seq, pos++, PyBool_FromLong(config->disable_gil));
     SetFlag(preconfig->utf8_mode);
 #undef SetFlag
 
