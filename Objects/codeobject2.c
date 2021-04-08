1d2cf7a5e8970d6ebfe0847ec92e97261edbe03b
Sam Gross <colesbury@gmail.com>
2021-04-08 15:03:13 -0700

    code: make repr look like regular code object

--- Objects/codeobject2.c
+++ Objects/codeobject2.c
@@ -280,11 +280,11 @@ code_repr(PyCodeObject2 *co)
         lineno = -1;
     if (co->co_filename && PyUnicode_Check(co->co_filename)) {
         return PyUnicode_FromFormat(
-            "<code2 object %U at %p, file \"%U\", line %d>",
+            "<code object %U at %p, file \"%U\", line %d>",
             co->co_name, co, co->co_filename, lineno);
     } else {
         return PyUnicode_FromFormat(
-            "<code2 object %U at %p, file ???, line %d>",
+            "<code object %U at %p, file ???, line %d>",
             co->co_name, co, lineno);
     }
 }
