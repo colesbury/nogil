84608e2a7d2da092f910e0dbf90a6a22485a1223
Sam Gross <colesbury@gmail.com>
2021-04-20 14:38:11 -0700

    use the correct dis module in test_positional_only_arg.py
    The tests still fail

--- Lib/test/test_positional_only_arg.py
+++ Lib/test/test_positional_only_arg.py
@@ -1,6 +1,10 @@
 """Unit tests for the positional only argument syntax specified in PEP 570."""
 
-import dis
+import sys
+if sys.use_new_bytecode():
+    import dis2 as dis
+else:
+    import dis
 import pickle
 import unittest
 
