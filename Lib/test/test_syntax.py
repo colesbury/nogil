20c9e3aede249891b445c8fc3a24efede618ae0c
Sam Gross <colesbury@gmail.com>
2021-04-22 11:10:22 -0700

    test_syntax: fix infinite loop
    Deeply nested blocks no longer raise a compile error, so what used to be
    a syntax error became an infinite loop. This rewrites the test so that
    it checks that deeply nested blocks work ok.

--- Lib/test/test_syntax.py
+++ Lib/test/test_syntax.py
@@ -421,34 +421,33 @@ isn't, there should be a syntax error.
 This raises a SyntaxError, it used to raise a SystemError.
 Context for this change can be found on issue #27514
 
-In 2.5 there was a missing exception and an assert was triggered in a debug
-build.  The number of blocks must be greater than CO_MAXBLOCKS.  SF #1565514
+Python previously had a limit of 20 on the number of nested blocks.
 
-   >>> while 1:
-   ...  while 2:
-   ...   while 3:
-   ...    while 4:
-   ...     while 5:
-   ...      while 6:
-   ...       while 8:
-   ...        while 9:
-   ...         while 10:
-   ...          while 11:
-   ...           while 12:
-   ...            while 13:
-   ...             while 14:
-   ...              while 15:
-   ...               while 16:
-   ...                while 17:
-   ...                 while 18:
-   ...                  while 19:
-   ...                   while 20:
-   ...                    while 21:
-   ...                     while 22:
-   ...                      break
-   Traceback (most recent call last):
-     ...
-   SyntaxError: too many statically nested blocks
+   >>> def f():
+   ...  while 1:
+   ...   while 2:
+   ...    while 3:
+   ...     while 4:
+   ...      while 5:
+   ...       while 6:
+   ...        while 8:
+   ...         while 9:
+   ...          while 10:
+   ...           while 11:
+   ...            while 12:
+   ...             while 13:
+   ...              while 14:
+   ...               while 15:
+   ...                while 16:
+   ...                 while 17:
+   ...                  while 18:
+   ...                   while 19:
+   ...                    while 20:
+   ...                     while 21:
+   ...                      while 22:
+   ...                       return 'ok'
+   >>> f()
+   'ok'
 
 Misuse of the nonlocal and global statement can lead to a few unique syntax errors.
 
