bf5615e0a67e3bd28b25b8af84f4569813fe01a0
Sam Gross <colesbury@gmail.com>
2021-08-24 13:12:43 -0700

    patchlevel: add PY_NOGIL define to mark "nogil" implementation

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -22,6 +22,8 @@
 #define PY_RELEASE_LEVEL        PY_RELEASE_LEVEL_ALPHA
 #define PY_RELEASE_SERIAL       4
 
+#define PY_NOGIL                1
+
 /* Version as a string */
 #define PY_VERSION              "3.9.0a4+"
 /*--end constants--*/
