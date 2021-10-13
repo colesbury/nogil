059d54835a6e9cfb20e340e5357dd7b3d6f628a9
Sam Gross <colesbury@gmail.com>
2021-10-13 08:33:46 -0700

    Move _Py_RefTotal to per-thread state
    When Py_REF_DEBUG is defined, Python aggregates the number of
    refcounting changes. This adds a per-thread counter and only aggregates
    across threads when _Py_GetRefTotal() is called. Threads also add their
    local counters to the global counter when they exit.
    counter on exit.

--- Include/internal/pycore_runtime.h
+++ Include/internal/pycore_runtime.h
@@ -102,6 +102,8 @@ typedef struct pyruntimestate {
     void *open_code_userdata;
     _Py_AuditHookEntry *audit_hook_head;
 
+    Py_ssize_t ref_total;
+
     // XXX Consolidate globals found via the check-c-globals script.
 } _PyRuntimeState;
 
@@ -118,6 +120,8 @@ PyAPI_FUNC(void) _PyRuntimeState_Fini(_PyRuntimeState *runtime);
 #ifdef HAVE_FORK
 PyAPI_FUNC(void) _PyRuntimeState_ReInitThreads(_PyRuntimeState *runtime);
 #endif
+
+PyAPI_FUNC(Py_ssize_t) _PyRuntimeState_GetRefTotal(_PyRuntimeState *runtime);
 
 /* Initialize _PyRuntimeState.
    Return NULL on success, or return an error message on failure. */
