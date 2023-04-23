colesbury/nogil-3.12

pystate: refcount threads to handle race between interpreter shutdown and thread exit

Include/internal/pycore_pystate.h
@@ -169,6 +169,8 @@ PyAPI_FUNC(PyThreadState *) _PyThreadState_UnlinkExcept(
     int already_dead);
 PyAPI_FUNC(void) _PyThreadState_DeleteGarbage(PyThreadState *garbage);
 
+extern void _PyThreadState_DecRef(PyThreadState *tstate);
+
 static inline void
 _PyThreadState_Signal(PyThreadState *tstate, uintptr_t bit)
 {

Python/ceval_gil.c
@@ -263,6 +263,7 @@ take_gil(PyThreadState *tstate)
            This code path can be reached by a daemon thread after Py_Finalize()
            completes. In this case, tstate is a dangling pointer: points to
            PyThreadState freed memory. */
+        _PyThreadState_DecRef(tstate);
         PyThread_exit_thread();
     }
 
@@ -310,6 +311,7 @@ take_gil(PyThreadState *tstate)
                     _PyThreadState_Unsignal(gil->holder, EVAL_DROP_GIL);
                 }
                 MUTEX_UNLOCK(gil->mutex);
+                _PyThreadState_DecRef(tstate);
                 PyThread_exit_thread();
             }
             assert(is_tstate_valid(tstate));
@@ -350,6 +352,7 @@ _ready:
            wait_for_thread_shutdown() from Py_Finalize(). */
         MUTEX_UNLOCK(gil->mutex);
         drop_gil(ceval, ceval2, tstate);
+        _PyThreadState_DecRef(tstate);
         PyThread_exit_thread();
     }
     assert(is_tstate_valid(tstate));

colesbury/nogil

Include/internal/pycore_pystate.h
@@ -169,6 +169,8 @@ PyAPI_FUNC(PyThreadState *) _PyThreadState_UnlinkExcept(
     int already_dead);
 PyAPI_FUNC(void) _PyThreadState_DeleteGarbage(PyThreadState *garbage);
 
+extern void _PyThreadState_Exit(PyThreadState *tstate);
+
 static inline void
 _PyThreadState_Signal(PyThreadState *tstate, uintptr_t bit)
 {

Python/ceval_gil.c
@@ -263,7 +263,7 @@ take_gil(PyThreadState *tstate)
            This code path can be reached by a daemon thread after Py_Finalize()
            completes. In this case, tstate is a dangling pointer: points to
            PyThreadState freed memory. */
-        PyThread_exit_thread();
+        _PyThreadState_Exit(tstate);
     }
 
     assert(is_tstate_valid(tstate));
@@ -310,7 +310,7 @@ take_gil(PyThreadState *tstate)
                     _PyThreadState_Unsignal(gil->holder, EVAL_DROP_GIL);
                 }
                 MUTEX_UNLOCK(gil->mutex);
-                PyThread_exit_thread();
+                _PyThreadState_Exit(tstate);
             }
             assert(is_tstate_valid(tstate));
 
@@ -350,7 +350,7 @@ _ready:
            wait_for_thread_shutdown() from Py_Finalize(). */
         MUTEX_UNLOCK(gil->mutex);
         drop_gil(ceval, ceval2, tstate);
-        PyThread_exit_thread();
+        _PyThreadState_Exit(tstate);
     }
     assert(is_tstate_valid(tstate));
 